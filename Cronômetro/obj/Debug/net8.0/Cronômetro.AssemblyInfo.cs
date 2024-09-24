e6c33b722ca7f782dd2acaa60d61654b78fe5ff1
Kauany Calais <kauanycalais10@gmail.com>
2024-09-24 14:51:19 -0300

    Adicionando atividade 2

--- Cronômetro/obj/Debug/net8.0/Cronômetro.AssemblyInfo.cs
+++ Cronômetro/obj/Debug/net8.0/Cronômetro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cronômetro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32fcdb2bbb027bfa7375ed4bfd223af3032ddefd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c4edf093820661256fd0487d446dbfb15a31706")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cronômetro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cronômetro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
